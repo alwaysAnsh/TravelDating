5bba91b4f3f00b82adf3bece90f7e08fc1bb9391
alwaysAnsh <anshj9y@gamil.com>
2024-09-10 15:26:32 +0530

    Not found page added

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div>
+      <div>
+        Not forund
+      </div>
+      <Link to="/">
+            <button className="bg-green-600 hover:bg-green-700 text-white font-semibold py-2 px-6 rounded-lg shadow-lg transition duration-300">
+              back to home
+            </button>
+          </Link>
+
+    </div>
   )
 }
 
