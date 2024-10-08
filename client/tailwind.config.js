b733d92e8cc946431e7536ec19a7f70f3379c6d9
alwaysAnsh <anshj9y@gamil.com>
2024-10-08 15:53:54 +0530

    some basic installation with changes in frontend

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryRed: "#E43D12",
+        secondaryRed: "#D6536D",
+        lightPink: "#FFA2B6",
+        yellowAccent: "#EFB11D",
+        offWhite: "#EBE9E1",
+      },
+      fontFamily: {
+        headline: ["Futura", "Roboto Condensed", "sans-serif"],
+        body: ["Roboto", "Helvetica", "sans-serif"],
+        "playfair": ["Playfair Display", 'serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
